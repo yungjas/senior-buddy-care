6d98fd3216bb886d0e5999f6a6c3d21d72749bb5
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-10-04 22:17:09 +0800

    cater to win32 and windows subsystem

--- tele_alerts.py
+++ tele_alerts.py
@@ -4,6 +4,9 @@ import serial.tools.list_ports as list_ports
 import requests
 import time
 import os
+import sys
+import subprocess
+import re
 
 PID_MICROBIT = 516
 VID_MICROBIT = 3368
@@ -16,25 +19,72 @@ CHAT_ID = os.getenv("CHAT_ID")
 app = flask.Flask(__name__)
 app.debug = True
 
+# Handles the case when the serial port can't be found
+def handle_missing_serial_port() -> None:
+    print("Couldn't connect to the micro:bit. Try these steps:")
+    print("1. Unplug your micro:bit")
+    print("2. Close Tera Term, PuTTY, and all other apps using the micro:bit")
+    print("3. Close all MakeCode browser tabs using the micro:bit")
+    print("4. Plug the micro:bit in")
+    print("5. Run this app again")
+    exit()
+
 
 def find_comport(pid, vid, baud):
-    ''' return a serial port '''
-    ser_port = serial.Serial(timeout=TIMEOUT)
-    ser_port.baudrate = baud
-    ports = list(list_ports.comports())
-    print('scanning ports')
-    for p in ports:
-        print('port: {}'.format(p))
+    if "win32" in sys.platform.lower():
+        ''' return a serial port '''
+        ser_port = serial.Serial(timeout=TIMEOUT)
+        ser_port.baudrate = baud
+        ports = list(list_ports.comports())
+        print('scanning ports')
+        for p in ports:
+            print('port: {}'.format(p))
+            try:
+                print('pid: {} vid: {}'.format(p.pid, p.vid))
+            except AttributeError:
+                continue
+            if (p.pid == pid) and (p.vid == vid):
+                print('found target device pid: {} vid: {} port: {}'.format(
+                    p.pid, p.vid, p.device))
+                ser_port.port = str(p.device)
+                return ser_port.port
+    
+    elif "microsoft" in os.uname().release.lower():
+        # List the serial devices available
         try:
-            print('pid: {} vid: {}'.format(p.pid, p.vid))
-        except AttributeError:
-            continue
-        if (p.pid == pid) and (p.vid == vid):
-            print('found target device pid: {} vid: {} port: {}'.format(
-                p.pid, p.vid, p.device))
-            ser_port.port = str(p.device)
+            stdout = subprocess.check_output("/mnt/c/windows/system32/WindowsPowerShell/v1.0/powershell.exe -Command '[System.IO.Ports.SerialPort]::getportnames()'", shell = True).decode("utf-8").strip()
+            if not stdout:
+                handle_missing_serial_port()
+        except subprocess.CalledProcessError as e:
+            print(f"Couldn't list serial ports: {e.output.decode('utf8').strip()}")
+            handle_missing_serial_port()
+
+        # Guess the serial device
+        stdout = stdout.splitlines()[-1]
+        ser_port = re.search("COM([0-9]*)", stdout)
+        if ser_port:
+            ser_port = f"/dev/ttyS{ser_port.group(1)}"
             return ser_port
-    return None
+    
+    elif "linux" in sys.platform.lower():
+        # List the serial devices available
+        try:
+            stdout = subprocess.check_output("ls /dev/ttyACM*", stderr=subprocess.STDOUT, shell = True).decode("utf-8").strip()
+            if not stdout:
+                handle_missing_serial_port()
+        except subprocess.CalledProcessError as e:
+            print(f"Couldn't list serial ports: {e.output.decode('utf8').strip()}")
+            handle_missing_serial_port()
+
+        # Guess the serial device
+        ser_port = re.search("(/dev/ttyACM[0-9]*)", stdout)
+        if ser_port:
+            ser_port = ser_port.group(1)
+            return ser_port
+    
+    else:
+        print(f"Unknown platform: {sys.platform}")
+        exit()
 
 
 def incoming_message(s):
@@ -49,7 +99,7 @@ def incoming_message(s):
 def tele_alert():
     serial_port = find_comport(PID_MICROBIT, VID_MICROBIT, BAUDRATE)
     
-    with serial.Serial(port=serial_port.port, baudrate=BAUDRATE, timeout=10) as s:
+    with serial.Serial(port=serial_port, baudrate=BAUDRATE, timeout=10) as s:
         # Sleep a while to make sure serial port is open before doing anything else
         time.sleep(1) 
 
