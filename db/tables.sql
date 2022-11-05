360cee0ef8c7262c1297b9c6f094666db6e62e00
Jasmine Lim <jasmine-lim88@hotmail.com>
2022-11-06 00:32:53 +0800

    added tilt to acceleration table

--- db/tables.sql
+++ db/tables.sql
@@ -1,20 +1,23 @@
+-- DB USES UTC TIME
+
 CREATE TABLE `acceleration` (
     `acc_id` int NOT NULL AUTO_INCREMENT,
-    `acc_data` float NOT NULL,
-    `time_created` DATETIME NULL,
+    `acc` float NOT NULL,
+    `tilt` varchar(64) NOT NULL,
+    `time_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`acc_id`)
 );
 
 CREATE TABLE `weight` (
     `weight_id` int NOT NULL AUTO_INCREMENT,
     `weight_data` float NOT NULL,
-    `time_created` DATETIME NULL,
+    `time_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`weight_id`)
 );
 
 CREATE TABLE `light` (
     `light_id` int NOT NULL AUTO_INCREMENT,
     `light_data` float NOT NULL,
-    `time_created` DATETIME NULL,
+    `time_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(`light_id`)
 );
